8310297c08c3517750ea0f6f0139a1cff1b8e594
KevinLiTian <kevin.li20021106@gmail.com>
2022-06-01 16:32:34 -0400

    Adding the contact page

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+function alertmsg() {
+  var name = document.getElementById("name");
+  var email = document.getElementById("email");
+  var message = document.getElementById("message");
+  if (name.value !== "" && email.value !== "" && message.value !== "") {
+    document.getElementById("alert").style.display = "block";
+  }
+}
+
