ece0697d8ad391cce1176ad90793db019eb8cd31
KevinLiTian <kevin.li20021106@gmail.com>
2022-06-02 10:44:47 -0400

    Add error handling in contact form alert msg

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ menu.addEventListener("click", function () {
 });
 
 function alertmsg() {
-  var name = document.getElementById("name");
-  var email = document.getElementById("email");
-  var message = document.getElementById("message");
-  if (name.value !== "" && email.value !== "" && message.value !== "") {
-    document.getElementById("alert").style.display = "block";
+  var name = document.forms["myform"]["Name"].value;
+  var email = document.forms["myform"]["Email"].value;
+  var message = document.forms["myform"]["Message"].value;
+  var valid = false;
+
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === "@") {
+      valid = true;
+    }
+  }
+
+  if (name !== "" && email !== "" && message !== "" && valid === true) {
+    document.getElementById("suc").style.display = "block";
+  } else {
+    document.getElementById("err").style.display = "block";
   }
 }
-
